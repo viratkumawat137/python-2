2790fbccaaf332faab75f3d60dff677e739b390c
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-27 23:56:55 +0530

    Add files via upload

---
+++ opraters.py
@@ -0,0 +1,47 @@
+# # 1. Arithmetic Operators
+# num1 = 40
+# num2 = 8
+
+
+# print(num1 - num2)       # subtraction
+# print(num1 * num2)       # multiplication
+
+
+
+
+# 2. Assignment Operators
+# value = 50
+# print(value)
+
+# value -= 10              # subtract and assign
+# print(value)
+
+
+# # 3. Comparison Operators
+# p1 = 18
+# p2 = 18
+# print(p1 == p2)           # less than comparison
+
+# p3 = 30
+# p4 = 25
+# print(p3 > p4)           # greater than comparison
+
+
+# # 4. Logical Operators
+# x1 = 5
+# x2 = 15
+# x3 = 25
+# x4 = 35
+
+# print("And result :", x1 < x2 and x3 > x4)   # AND condition
+# print("OR Result :", x1 < x2 or x3 > x4)  # OR condition  esko ek chiz true caheyee bs 
+
+
+# # 5. Identity Operators -
+# m1 = 100
+# m2 = 100
+# print(m1 is m2)
+
+
+# 6. Membership Operators
+# print("Check 'a' in 'mango' :", 'a' in 'mango')
