c2f9bd62499335045543a130e86fa4f7023bb7e4
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-27 15:27:32 +0530

    data type python-2

---
+++ data_types.py
@@ -0,0 +1,106 @@
+# # Learnign Data Types in Python
+# # 7 Data types
+
+
+# 1.Text Data type1
+# String
+# customer_name = "Rohit"
+# print("Customer Name is :", customer_name)
+# print("Customer Datatype is :", type(customer_name))
+
+
+
+
+# 2. Numeric Data Typle
+#2.1-Integer-complete number
+# rating = 4
+# order_quantity = 3
+
+# print("Rating data type", type(rating))
+# print("Order_Quantity data type :", type(order_quantity))
+
+
+
+
+
+
+#2.2-Float - Decimal number
+# order_amount = 8599.90
+
+# print("order_amount data type :", type(order_amount))
+
+
+
+# #2.3 Complex Number
+# a=3+4j
+# print(a)
+# print(type(a))
+
+
+
+
+# # 3.Boolean - Ture/False
+# a= True
+
+# print(a, type(a))
+
+
+
+
+# 4. Sequence
+# 4.1 - List
+# cities = ["Mumbai", "Delhi", "Pune", "Chennai"]
+# print(cities)
+# print(type(cities))
+
+
+
+
+# 4.2 - Tuple
+# dimensions = (1920,1921)
+# print(dimensions)
+# print(type(dimensions))
+
+
+
+
+
+# # 4.3 - Range
+# num = range(2,11)
+# print(list(num))   # [0,1,2,3,4]
+# print(type(num))
+
+
+
+
+# days = range(1, 31)  # 1 to 30 days
+# print(list(days))
+
+
+# # 5. Dictionary(dict)
+# student = {
+#     "name": "Anvi",
+#     "age": 5,
+#     "city": "Mumbai"
+# }
+# print(student)
+# print(type(student))
+
+
+
+
+
+# 6. Set
+# numbers = {1, 2, 2, 3, 4}
+# print(numbers)      # Output: {1, 2, 3, 4}
+
+# print(type(numbers))
+
+
+
+# 7.NoneType - No Value
+# remarks = None
+# print(remarks, type(remarks))
+
+
+
