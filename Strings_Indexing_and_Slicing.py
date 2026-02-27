5f2091ef1cab593f60c5382360ce7007fad49257
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-28 01:35:21 +0530

    Add files via upload

---
+++ Strings_Indexing_and_Slicing.py
@@ -0,0 +1,39 @@
+# String Indexing
+
+# name="satish"
+# print(name)
+# print(name[0])
+# print(name[-4])
+
+# # String Slicing
+# product="Laptop pro 2024"
+# print(product[-4:])
+
+
+
+text = "DataAnalysis"
+# ** Extracting first 4 characters **
+# print("First 4 letters:", text[0:4])    # Data
+# 0: Jahan se shuru karna hai (D se).
+# 4: Jahan tak jana hai (lekin 4th index wala character count nahi hoga).
+
+
+# # Extracting characters from middle
+# print("Middle slice:", text[4:12])      # Analysis
+
+# # Extract till end
+# print("Till end:", text[4:])            # Analysis
+
+# # Extract from beginning
+# print("From start:", text[:4])          # Data
+
+# # Extract last 5 characters
+# print("Last 5 letters:", text[-5:])     # alysis
+
+#Skip Text "DataAnalysis"
+# print("Skip Text :", text[0:12:3])  #daas
+# print("Reverse :", text[::-1])    #sisylanAataD
+
+
+
+
