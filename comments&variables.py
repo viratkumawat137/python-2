f8181a0e7c1d44566eea84b0af409eaa6ee475e1
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-27 08:50:29 +0530

    Add files via upload

---
+++ comments&variables.py
@@ -0,0 +1,27 @@
+
+                                          # use vs code triks for python programming  
+
+#**CLS**===>agr terminal pr lines ko clean krni h toh ""cls "" ka use kr skte hai! 
+# **ctrl + /** ===>se hum kisi bhi line ko comment kr skte hai caheye wo multiple lines ho ya single line ho !
+ #**ALT +SHIFT + a**==>  multiply line ko comment krne kee leye use krte hai !==>"""(yee vala comment)""""
+# "ctrl + alt+N " ==>code ko run kerwane kee leye shortkey 
+# auto save ==>"ctrl + s" see hum apne code ko save kr skte hai !
+# file pr clcik krke auto save ko on krne see file automatically save ho jayegi jab bhi hum code me changes karenge !
+#**ALT + SHIFT + F**==> CODE KO FORAMTE M KRNE KEE LEYEE USE KRTE HAI !
+# variables ==> yee data ko store krwata hai ek contaner m! example =>  a=int(input("sahil kumawat "))
+"""
+name= ("sahil kumawat")
+salary=245004
+age=23
+print( "welcome" ,name )
+print( "salary=" ,salary )
+print( "age=" ,age )"""  
+
+
+
+
+
+
+
+
+
