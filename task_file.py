420eec98ce1fec869fc2bd6fded1ad2d9527e186
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-28 13:31:46 +0530

    Add files via upload

---
+++ task_file.py
@@ -0,0 +1,58 @@
+# # 1. SOLUTION (Python Program Answer)
+
+# security_code = int(input("Enter the security code: "))
+
+# if security_code == 5566:
+#     department = input("Enter your department: ")
+
+#     if department.lower() == "finance":
+#         access_level = int(input("Enter your access level: "))
+
+#         if access_level >= 5:
+#             print("Access Granted: Welcome to the meeting room.")
+#         else:
+#             print("Insufficient access level.")
+#     else:
+#         print("Access denied: Department not allowed.")
+# else:
+#     print("Invalid security code.")
+
+
+# # ASSIGNMENT SET 2 — Online Exam Login
+
+# reg = int(input("Enter registration number: "))
+
+# if reg == 1221:
+#     subject = input("Enter exam subject: ")
+
+#     if subject.lower() == "python":
+#         password = int(input("Enter exam password: "))
+
+#         if password == 8888:
+#             print("Login successful! Start your exam.")
+#         else:
+#             print("Wrong password.")
+#     else:
+#         print("Subject not available.")
+# else:
+#     print("Registration failed.")
+
+
+
+# #3rd fun task===>
+# a=int(input("code bta muna"))
+# if a == 2002:
+#     print("come buddy")
+# else:
+#     print("bhg ja eder see")
+# b=(input(" pesa laya hai "))
+# if b =="yes":
+#     print("good")
+# else:
+#     print("goli maar esko ")
+
+# c=int(input(" kitna laya hai? "))
+# if c >=10000:
+#     print("ho jayega kaam")
+# else:
+#     print("babu bhai naam h mera ")
