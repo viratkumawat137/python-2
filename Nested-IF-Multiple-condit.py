70c22e1151f69cb4875239dd4f22c5e1dbb38916
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-28 11:09:02 +0530

    Add files via upload

---
+++ Nested-IF-Multiple-condit.py
@@ -0,0 +1,51 @@
+# 9-Python-Nested IF and Multiple conditions
+
+# print("Check your Elligibility")
+# age=int(input("Enter your age :"))
+# if age>=18:
+#    id_no=int(input("Enter your id no :")) 
+#    if id_no <=13:
+#       print(" you can enter")
+#    else:
+#       print("Wrong ID Number")
+# else:
+#    print("you are underage.")
+
+
+## Multiple conditions (and)
+# age=int(input("Enter your age :"))
+# residence=input("Are you Indian? : ")
+# if age>=18 and residence.lower()=="yes":
+#     print("Eligible to drive")
+# else:
+#     print("Not Eligible to drive")
+
+
+
+# # Multiple conditions (OR)                    ======> ek condition true honi caheyee bs fer aapka kaam done hai 
+# age=int(input("Enter your age :"))
+# residence=input("do you have licence : ")
+
+# if age>=18 or residence.lower()=="yes":
+#     print("Eligible to register")
+# else:
+#     print("Not Eligible to register")
+
+
+
+# print("The CHILD=>gun caheyee")
+# print("mil jayegi yee bta ")
+# age=int(input("Enter your age :"))
+# money=input("pesa laya hai: ")
+# a=int(input("kitna laya hai jayda lhgega : "))
+
+# if age>=18 or money.lower()=="yes" or a>=10000:
+#     print("Eligible to register")
+# else:
+#     print("Not Eligible to register")
+
+
+
+
+
+
