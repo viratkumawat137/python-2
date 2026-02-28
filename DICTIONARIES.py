7d567d2c3b0c37a9ae83434e69346860b0400d80
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-28 23:53:08 +0530

    Add files via upload

---
+++ DICTIONARIES.py
@@ -0,0 +1,44 @@
+# # DAY 15 – DICTIONARIES 
+
+
+student = {"name": "Rohit", "age": 21, "city": "Pune"}
+# print(student)
+
+
+# # Accessing values
+# print(student["name"])
+# print(student["city"])
+
+# # Adding and updating
+# student["marks"] = 85
+# student["city"] = "Mumbai"
+# print(student)
+
+# Removing
+# student.pop("age")
+# print(student)
+
+# Dictionary methods
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+# print(student.get("name"))
+
+# # Looping
+# for k in student:
+#     print(k, student[k])
+
+# Nested dictionary
+# employees = {
+#     "E101": {"name": "Rohit", "city": "Pune"},
+#     "E102": {"name": "Sneha", "city": "Delhi"}
+# }
+# print(employees["E101"]["name"])
+
+# Mapping wrong → correct
+# mapper = {
+#     "mombai": "Mumbai",
+#     "kolkatta": "Kolkata"
+# }
+# print(mapper["mombai"])
+
