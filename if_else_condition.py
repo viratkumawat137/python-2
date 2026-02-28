35c32c34c8954bf2460712ed49a5960d3f2eab30
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-28 09:30:41 +0530

    Add files via upload

---
+++ if_else_condition.py
@@ -0,0 +1,74 @@
+# #  IF ELSE CONDITIONS
+
+
+# age=int(input("Enter Your age:"))
+# if age>=18:
+#     print("Voting Qualified")
+# else:
+#     print("Not Quanlified for Voting")
+
+
+
+# #  Discount Checker
+# amount = 1500
+# if amount >= 1000:
+#     print("You get a discount!")
+# else:
+#     print("No discount")
+
+
+#  #if–elif–else (Multiple Conditions)
+# marks = int(input("Enter your Marks :"))
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 60:
+#     print("Grade B")
+# elif marks >= 35:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+
+# # Sales Performance (Data Analyst Use Case)
+# sales = (input("Enter Your sales amount:"))
+# if sales >= 100000:
+#     print("High Performer")
+# elif sales >= 50000:
+#     print("Medium Performer")
+# else:
+#     print("Low Performer")
+
+
+ #String Comparison Example
+# city = (input("Enter Your city:"))
+# if city == "delhi":
+#     print("City matched")
+# else:
+#     print("City not matched")
+
+
+
+# Password Program
+# password=input("Enter your password :")
+# if password.lower()=="admin@1": #LOWER()==> YEE CAPITAL KO BHI SMALL KR DEGA
+#     print("Login successful")
+# else:
+#     print("Wrong Password")
+
+
+
+# # Email Validation
+
+# email = input("Enter your Email :")
+# if "@" in email and "." in email:
+#     print("Valid Email")
+# else:
+#     print("Invalid Email")
+
+
+#  Advanced: Missing Data Check
+# value = input("Enter your data :")
+# if value == "sdf":
+#     print("Missing Data Found")
+# else:
+#     print("Data Available")
